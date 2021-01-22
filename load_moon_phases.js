2aab25c953a40c0c997c287764cfa2d7dd9987c8
AGUERY <43554490+AGUERY@users.noreply.github.com>
2021-01-22 11:24:21 +0100

    Add files via upload

---
+++ load_moon_phases.js
@@ -0,0 +1,16 @@
+function load_moon_phases(obj,callback){
+    var gets=[]
+    for (var i in obj){
+        gets.push(i + "=" +encodeURIComponent(obj[i]))
+    }
+    gets.push("LDZ=" + new Date(obj.year,obj.month-1,1) / 1000)
+    var xmlhttp = new XMLHttpRequest()
+    var url = "https://www.icalendar37.net/lunar/api/?" + gets.join("&")
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            callback(JSON.parse(xmlhttp.responseText))
+        }
+    }
+    xmlhttp.open("GET", url, true)
+    xmlhttp.send()
+}
